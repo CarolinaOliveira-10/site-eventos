636aac2f7d7adc47e394fef25605bc482d375602
oliveira.carolina <oliveira.carolina@localiza.com>
2023-04-18 16:50:38 -0300

    feat: exemplos de boas práticas em JS

--- js/pagina-inicial.js
+++ js/pagina-inicial.js
@@ -1,17 +1,38 @@
-// Deixar o nome de funções claras
+//////////// Deixar o nome de funções claras
 aoClicarParaMarcarEvento();
 
-function aoClicarParaMarcarEvento(){
-console.log('evento marcado');
+function aoClicarParaMarcarEvento() {
+console.log('Evento marcado');
 }
 
 
-// Verificação de equidade de valores
+//////////// Falsy
+// Valores que são considerados falsy (false, null, undefined, 0, NaN e '')
+falsy();
+function falsy(){
+    console.log("Entrou na função");
+    if (null) {
+        console.log("Entrou no if");
+    }
+}
+
+
+//////////// Verificação de igualdade e desigualdade de valores
+// tipos iguais
+console.log("1", 18 === 18);
+console.log("2", 'carol' === 'carol');
+
+// tipos diferentes
+console.log("3",'0' ===  0);
+console.log("4",0 === false);
+
+//
+console.log("5",'0' !==  0);
+console.log("6",0 !== false);
 
 
 
-
-// Declarações de informações únicas em um mesmo contexto.
+//////////// Declarações de informações únicas em um mesmo contexto.
 var numeros = [1,2,3];
 for(var i = 0; i < numeros.length; i++) {
     const multiplicador = 2;
@@ -26,3 +47,28 @@ for(var i = 0; i < numeros.length; i++) {
     const somaNumeroMaisPosicao = numeros[i] * multiplicador;
     console.log(somaNumeroMaisPosicao);
 }
+
+
+
+//////////// Hoisting
+
+exibirNomeONG();
+function exibirNomeONG() {
+    var nomeONG = 'WoMakersCode';
+    console.log(nomeONG);
+}
+
+//
+console.log(nome);
+var nome = 'WoMakersCode';
+
+
+// 
+console.log(nome); 
+var nome;
+nome = 'WoMakersCode';
+
+
+nome = 'WoMakersCode';
+console.log(nome);
+var nome;
