97d93c31c82cddf6bb953b4d8c1fa629b5391f5a
oliveira.carolina <oliveira.carolina@localiza.com>
2023-04-18 01:34:30 -0300

    feat: tela com boas praticas

--- js/pagina-inicial.js
+++ js/pagina-inicial.js
@@ -1,5 +1,31 @@
-
+// Deixar o nome de funções claras
+aoClicarParaMarcarEvento();
 
 function aoClicarParaMarcarEvento(event){
-
+console.log('Teste')
 }
+
+
+// Verificação de equidade de valores
+
+
+
+
+// Declarações de informações únicas em um mesmo contexto.
+
+
+
+
+
+// 
+
+
+// this script used to check if the number is even or odd
+
+const num = 6;
+
+const result = (num % 2  != 0) ? "odd" : "even";
+
+// display the result
+
+console.log(`Number is ${result}.`);
